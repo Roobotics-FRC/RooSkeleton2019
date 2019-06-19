1b25b84cd6ff453180e4277e70f1f11e421cc425
aaplmath <aaplmath@users.noreply.github.com>
2019-06-19 18:26:01 -0400

    Add skeleton from 2019 season

---
+++ src/main/java/frc/team4373/robot/RobotMap.java
@@ -0,0 +1,11 @@
+package frc.team4373.robot;
+
+/**
+ * Holds various mappings and constants.
+ */
+
+public class RobotMap {
+    // OI devices
+    public static final int DRIVE_JOYSTICK_PORT = 0;
+    public static final int OPERATOR_JOYSTICK_PORT = 1;
+}
